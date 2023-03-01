ee4d783d84f39eb18b2f3620d01f40f0afbb7637
winglovecola@gmail.com <winglovecola@gmail.com>
2023-03-01 00:10:36 -0500

    updated connection.js and .env

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      host: 'localhost',
       dialect: 'mysql',
       port: 3306
     }
